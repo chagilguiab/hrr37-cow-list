e7d8ac6e17f8a8c80980b91d17120f413060852b
Chagil Guiab <chagilguiab@gmail.com>
2019-03-23 23:08:00 -0700

    complete cow app

--- client/src/components/Cow.jsx
+++ client/src/components/Cow.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 class Cow extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      name: this.props.cowName,
+      desc: this.props.cowDesc
+    }
   }
 
   render() {
     return (
-    <div>
+    <div onClick={()=> this.props.setDisplay(this.state.name, this.state.desc)}>
     <h1>{this.props.cowName}</h1>
     </div>
     )
